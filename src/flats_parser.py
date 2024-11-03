df29558ec587adf8bacc2fb706cb3eb05cc608d1
Dmitriy Lisitsyn <dmitriylisitsyn@Dmitriys-MacBook-Air.local>
2024-11-04 01:06:09 +0400

    flats update

--- src/flats_parser.py
+++ src/flats_parser.py
@@ -1,21 +1,34 @@
-from config import requests, json
+from src.config import requests, json
 
 
 def parse_flats():
-
     page_limit = 20
-
     offset = 0
     url = "https://a101.ru/api/v2/flat/?ordering=actual_price"
+
     response = requests.get(url)
 
-    if response.status_code == 200:
-        with open("b.json", 'w') as file:
-            json.dump(response.json(), file)
-            print("Success")
-    else:
-        print(f'Error: {response.status_code}')
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        exit(1)
 
-    # with open('b.json', 'r') as file:
-    #     data = json.load(file)
-    #     print(data)
+    data = response.json()
+
+    flats = []
+
+    while data['next'] is not None:
+        cur_flats = data['results']
+        flats.extend(cur_flats)
+
+        offset += page_limit
+        url = f"https://a101.ru/api/v2/flat/?ordering=actual_price&limit={page_limit}&offset={offset}"
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}")
+            exit(1)
+
+        data = response.json()
+
+        print(len(flats))
